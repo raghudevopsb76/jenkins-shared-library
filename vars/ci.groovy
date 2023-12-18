164632d9d5935e476be0096d2df5d969db26188d
Raghu K <raghuk.vit@gmail.com>
2023-12-18 07:13:22 +0530

    TF Code

---
+++ vars/ci.groovy
@@ -0,0 +1,36 @@
+def call() {
+  pipeline {
+    agent {
+      node { label 'workstation'}
+    }
+
+    stages {
+
+      stage('Compile') {
+        steps {
+          echo 'Compile'
+        }
+      }
+
+      stage('Test Cases') {
+        steps {
+          echo 'Test Cases'
+        }
+      }
+
+      stage('Build') {
+        steps {
+          echo 'Build'
+        }
+      }
+
+      stage('Release') {
+        steps {
+          echo 'Release'
+        }
+      }
+
+    }
+
+  }
+}
