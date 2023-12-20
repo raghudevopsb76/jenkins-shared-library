6f2d0435e829013a9342681695ae214824ae6dd4
Raghu K <raghuk.vit@gmail.com>
2023-12-20 07:22:08 +0530

    TF Code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,5 +1,8 @@
 def call() {
   node('workstation') {
+
+    sh "find . | sed -e '1d' |xargs rm -rf"
+
     if(env.TAG_NAME ==~ ".*") {
       env.branchName = env.TAG_NAME
     } else {
@@ -12,7 +15,7 @@ def call() {
           userRemoteConfigs: [[url: "https://github.com/raghudevopsb76/expense-backend"]]
       )
     }
-    sh 'ls'
+
     stage('Compile') {}
 
 
