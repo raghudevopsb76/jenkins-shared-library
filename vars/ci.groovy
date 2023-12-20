9418de90a5efce13f71e6655e9ea5f10e9466774
Raghu K <raghuk.vit@gmail.com>
2023-12-20 06:43:35 +0530

    TF Code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,25 +1,22 @@
 def call() {
   node('workstation') {
-    sh 'env'
+    stage('Code Checkout') {}
+    stage('Compile') {}
+
+
     if(BRANCH_NAME == "main") {
-      stage('Code Checkout') {}
-      stage('Compile') {}
       stage('Build') {}
     } else if(BRANCH_NAME ==~ "PR.*" ) {
-      stage('Code Checkout') {}
-      stage('Compile') {}
       stage('Test Cases') {}
       stage('Integration Test Cases') {}
     } else if (TAG_NAME ==~ ".*") {
-      stage('Code Checkout') {}
-      stage('Compile') {}
       stage('Build') {}
       stage('Release App') {}
     } else{
-      stage('Code Checkout') {}
-      stage('Compile') {}
       stage('Test Cases') {}
     }
+
+
 //    stage('Code Checkout') {}
 //    stage('Compile') {}
 //    stage('Test Cases') {}
