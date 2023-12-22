fd3f8c9e1b84a0b1a4b4734e6003ce1fb3bb4737
Raghu K <raghuk.vit@gmail.com>
2023-12-22 07:05:16 +0530

    TF Code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -43,7 +43,7 @@ def call() {
       stage('Code Quality') {
         env.SONAR_TOKEN = AWS_SSM_PARAM('sonar.token')
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_TOKEN}", var: 'PASSWORD']]]) {
-          sh 'sonar-scanner -Dsonar.host.url=http://172.31.85.179:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=expense-backend'
+          sh 'sonar-scanner -Dsonar.host.url=http://172.31.85.179:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=${repo_name} -Dsonar.qualitygate.wait=true'
         }
       }
 
