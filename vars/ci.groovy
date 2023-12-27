88441e51451fe4a78233ff7c3c0137396815cb5d
Raghu K <raghuk.vit@gmail.com>
2023-12-27 07:21:11 +0530

    TF Code

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -52,8 +52,15 @@ def call() {
       stage('Build') {}
     } else if (env.TAG_NAME ==~ ".*") {
       sh 'echo TAG'
-      stage('Build') {}
-      stage('Release App') {}
+      stage('Build') {
+        sh 'zip -r ${repo_name}-${TAG_NAME}.zip *'
+      }
+      stage('Release App') {
+        env.ARTIFACTORY_PASSWORD = AWS_SSM_PARAM('artifactory.password')
+        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${ARTIFACTORY_PASSWORD}", var: 'PASSWORD']]]) {
+          sh 'curl -sSf -u "admin:${ARTIFACTORY_PASSWORD}" -X PUT  -T ${repo_name}-${TAG_NAME}.zip http://artifactory.rdevopsb72.online:8081/artifactory/${repo_name}/${repo_name}-${TAG_NAME}.zip'
+        }
+      }
     } else{
       sh 'echo branch'
       stage('Test Cases') {
