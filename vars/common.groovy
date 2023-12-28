52a043ab476e8a847b928e354b39678a4b040f06
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-28 06:34:26 +0530

    TF Code

---
+++ vars/common.groovy
@@ -0,0 +1,21 @@
+def codeCheckour() {
+  sh "find . | sed -e '1d' |xargs rm -rf"
+
+  if(env.TAG_NAME ==~ ".*") {
+    env.branch_name = "refs/tags/${env.TAG_NAME}"
+  } else {
+    if (env.BRANCH_NAME ==~ "PR-.*") {
+      env.branch_name = "${env.CHANGE_BRANCH}"
+    } else {
+      env.branch_name = "${env.BRANCH_NAME}"
+    }
+  }
+
+  stage('Code Checkout') {
+    //git branch: "${env.branchName}", url: 'https://github.com/raghudevopsb76/expense-backend'
+    checkout scmGit(
+        branches: [[name: "${branch_name}"]],
+        userRemoteConfigs: [[url: "https://github.com/raghudevopsb76/${repo_name}"]]
+    )
+  }
+}
